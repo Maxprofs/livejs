63d47dc427c59d7edb2a7a9fed5cfef1fb02c96a
Gilbert Wong <gilbertwong@Gilberts-MBP-3.fios-router.home>
2017-06-25 23:32:28 -0700

    updated config file

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,3 @@
 module.exports.configServer = () => {
-    process.env.MONGOOSE_CONNECT = "mongodb://admin:forehandslap00@ds135592.mlab.com:35592/twitch-clone"
+    process.env.MONGOOSE_CONNECT = "add your connection here"
 }
